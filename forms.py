fa62cd3872b071988df20b5c391c47039f1be898
MarkSachinPerera <marksachin12@gmail.com>
2020-09-29 21:55:04 -0600

    Activity 2: Email throws error

--- forms.py
+++ forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, Regexp
 from wtforms import StringField, SubmitField
 
 class submitForm(FlaskForm):
     name = StringField("Enter your name?", validators=[DataRequired()])
-    email = StringField("Enter your U of T email address?", validators=[Email()])
+    email = StringField("Enter your UofT email address?", validators=[Email(granular_message=True)])
     submit = SubmitField("Submit")
