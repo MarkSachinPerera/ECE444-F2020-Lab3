168b3c7ef57ee6662042747f7db32bb871e6bca2
MarkSachinPerera <marksachin12@gmail.com>
2020-09-29 22:22:06 -0600

    Activity 2: completed

--- hello.py
+++ hello.py
@@ -3,13 +3,17 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from forms import submitForm
+import re
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "LOLLOL"
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/', methods=['POST','GET'])
+uoftemail = re.compile('.*(utoronto.ca)')
+
+
+@app.route('/', methods=['POST', 'GET'])
 @app.route('/index', methods=['POST', 'GET'])
 def index():
     form = submitForm()
@@ -17,16 +21,21 @@ def index():
         old_name = session.get('name')
         old_email = session.get('email')
 
-        if old_name is not None and form.name.data != old_name: 
+        if old_name is not None and form.name.data != old_name:
             flash("Looks like you changed your name!")
         if old_email is not None and old_email != form.email.data:
             flash("Looks like you changed your email!")
-            
+
         session['name'] = form.name.data
-        session['email'] = form.email.data
-        return redirect(url_for('index'))    
+
+        if uoftemail.match(form.email.data):
+            session['email'] = form.email.data
+        else:
+            session['email'] = None
+
+        return redirect(url_for('index'))
     return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'), email=session.get('email'))
-    
+
 
 @app.route('/user/<name>')
 def user(name):
