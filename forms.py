2a96a056b0fd073d0568af8c7c46f3fd2abf6fe6
MarkSachinPerera <marksachin12@gmail.com>
2020-09-29 22:38:18 -0600

    small fix

--- forms.py
+++ forms.py
@@ -3,6 +3,6 @@ from wtforms.validators import DataRequired, Email, Regexp
 from wtforms import StringField, SubmitField
 
 class submitForm(FlaskForm):
-    name = StringField("Enter your name?", validators=[DataRequired()])
-    email = StringField("Enter your UofT email address?", validators=[Email(granular_message=True)])
+    name = StringField("What is your name?", validators=[DataRequired()])
+    email = StringField("What is your UofT email address?", validators=[Email(granular_message=True)])
     submit = SubmitField("Submit")
